5b0146a9e529d5edfcc3c9bb8be28ac9248d3494
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2020-05-22 20:29:49 +1000

    Add project files.

---
+++ GmailApi/Controllers/GmailReaderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GmailApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GmailReaderController : ControllerBase
+    {
+
+        private readonly ILogger<GmailReaderController> _logger;
+
+        public GmailReaderController(ILogger<GmailReaderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
